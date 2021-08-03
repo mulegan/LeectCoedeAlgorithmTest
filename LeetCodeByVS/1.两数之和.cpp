1a4ba7a8a43fddf2bb1ebe1c4b94e490fedd77ea
mulegan <1473609368@qq.com>
2021-08-03 10:43:56 +0800

    Create 1.两数之和.cpp
    两数之和

---
+++ LeetCodeByVS/1.两数之和.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include<unordered_map>
+#include<vector>
+#include<iostream>
+
+using namespace::std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> Temp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (Temp.find(target - nums[i]) !=Temp.end())
+            {
+                return{Temp[target - nums[i]],i};
+            }
+            
+            Temp[nums[i]]=i;
+        }
+         return{};
+    }
+};
+// @lc code=end
+

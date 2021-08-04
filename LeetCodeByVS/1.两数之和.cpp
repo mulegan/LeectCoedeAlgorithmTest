b72ac156e48ac4ffe6ede3fe1d5827a09a7d53b3
mulegan <1473609368@qq.com>
2021-08-04 15:59:13 +0800

    使用插件整理

--- LeetCodeByVS/1.两数之和.cpp
+++ LeetCodeByVS/1.两数之和.cpp
@@ -5,27 +5,29 @@
  */
 
 // @lc code=start
-#include<unordered_map>
-#include<vector>
-#include<iostream>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
 
-using namespace::std;
+using namespace ::std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> Temp;
-        for (int i = 0; i < nums.size(); i++)
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> Temp;
+        for (auto i = 0; i != nums.size(); i++)
         {
-            if (Temp.find(target - nums[i]) !=Temp.end())
+            if (Temp.find(target - nums[i]) != Temp.end())
             {
-                return{Temp[target - nums[i]],i};
+                return {Temp[target - nums[i]], i};
             }
-            
-            Temp[nums[i]]=i;
+
+            Temp[nums[i]] = i;
         }
-         return{};
+        return {};
     }
 };
-// @lc code=end
 
+// @lc code=end
